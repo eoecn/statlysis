1e4017e0ba5252d35a58959ee2acf690d4ecbff0
David Chen <mvjome@gmail.com>
2013-07-18 17:36:51 +0800

    Use dataset's class type to judge

--- lib/statlysis/utils.rb
+++ lib/statlysis/utils.rb
@@ -3,6 +3,10 @@
 module Statlysis
   module Utils
     class << self
+
+      def is_activerecord?(data); data.is_a?(ActiveRecordDataset) end # || !!data.included_modules.index(ActiveRecord::Store) end
+      def is_mongoid?(data); data.is_a?(MongoidDataset) end # || !!data.included_modules.index(Mongoid::Document) end
+
     end
   end
 end
