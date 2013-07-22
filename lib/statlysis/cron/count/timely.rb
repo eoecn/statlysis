e2585c6a4c657cb07c1659e979418ac5e8ad3c9c
David Chen <mvjome@gmail.com>
2013-07-22 15:36:45 +0800

    Calculate multiple sources

--- lib/statlysis/cron/count/timely.rb
+++ lib/statlysis/cron/count/timely.rb
@@ -4,7 +4,7 @@ module Statlysis
   class Timely < Count
     def setup_stat_table
       # TODO migration proc, merge into setup_stat_table_and_model
-      cron.stat_table_name = [cron.class.name.split("::")[-1], cron.source_name, cron.source_where_array.join, cron.time_unit[0]].map {|s| s.to_s.gsub('_','') }.reject {|s| s.blank? }.join('_').downcase
+      cron.stat_table_name = [cron.class.name.split("::")[-1], cron.multiple_dataset.name, cron.source_where_array.join, cron.time_unit[0]].map {|s| s.to_s.gsub('_','') }.reject {|s| s.blank? }.join('_').downcase
       raise "mysql only support table_name in 64 characters, the size of '#{cron.stat_table_name}' is #{cron.stat_table_name.to_s.size}. please set cron.stat_table_name when you create a Cron instance" if cron.stat_table_name.to_s.size > 64
       unless Statlysis.sequel.table_exists?(cron.stat_table_name)
         Statlysis.sequel.transaction do
@@ -26,12 +26,18 @@ module Statlysis
 
     def output
       @output ||= (cron.time_range.map do |time|
-        timely_c  = cron.source.where(unit_range_query(time)).count
-        _t = DateTime1970
-        _t = is_time_column_integer? ? _t.to_i : _t
-        totally_c = cron.source.where(unit_range_query(time, _t)).count
+        timely_c = 0
+        totally_c = 0
+        # support multiple data sources
+        cron.multiple_dataset.sources.each do |s|
+          timely_c  += s.where(unit_range_query(time)).count
+          _t = DateTime1970
+          _t = is_time_column_integer? ? _t.to_i : _t
+          totally_c += s.where(unit_range_query(time, _t)).count
 
-        logger.info "#{time.in_time_zone} #{cron.source_name} timely_c:#{timely_c} totally_c:#{totally_c}"
+          logger.info "#{time.in_time_zone} #{cron.multiple_dataset.name} timely_c:#{timely_c} totally_c:#{totally_c}"
+        end
+
         if timely_c.zero? && totally_c.zero?
           nil
         else
