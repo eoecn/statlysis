cc36c6053af64f311cf3d67e239e6af1259af75e
David Chen <mvjome@gmail.com>
2013-07-23 17:48:25 +0800

    Fix Utils judge which ORM, Object#try may not defined

--- lib/statlysis/utils.rb
+++ lib/statlysis/utils.rb
@@ -3,8 +3,8 @@
 module Statlysis
   module Utils
     class << self
-      def is_activerecord?(data); data.is_a?(ActiveRecordDataset) || !!((data.try(:included_modules) || []).index(ActiveRecord::Store)) end
-      def is_mongoid?(data); data.is_a?(MongoidDataset) || !!((data.try(:included_modules) || []).index(Mongoid::Document)) end
+      def is_activerecord?(data); data.is_a?(ActiveRecordDataset) || !!((data.respond_to?(:included_modules) ? data.included_modules : []).index(ActiveRecord::Store)) end
+      def is_mongoid?(data); data.is_a?(MongoidDataset) || !!((data.respond_to?(:included_modules) ? data.included_modules : []).index(Mongoid::Document)) end
       def name(data)
         return :collection_name if Utils.is_mongoid?(data)
         return :table_name      if Utils.is_activerecord?(data)
